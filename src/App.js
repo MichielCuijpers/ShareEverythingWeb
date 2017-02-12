a151297ee9109f1e2b11c0e2d7594292f94480e5
Casper Kuijjer <casper@kuijjer.com>
2017-02-12 10:04:17 +0100

    ESLint fix

--- src/App.js
+++ src/App.js
@@ -18,52 +18,47 @@ import OwnerModel from './models/OwnerModel';
 import rootReducer from './reducers';
 import customTheme from './customTheme';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  onRouteUpdate = () => {
+const App = () => {
+  const onRouteUpdate = () => {
     window.scrollTo(0, 0);
   };
 
-  render() {
-    injectTapEventPlugin();
-    const muiTheme = getMuiTheme(customTheme);
+  injectTapEventPlugin();
+  const muiTheme = getMuiTheme(customTheme);
 
-    const store = createStore(
-      rootReducer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
-    OwnerModel.load(store.dispatch)
+  const store = createStore(
+    rootReducer,
+    // eslint-disable-next-line
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+  OwnerModel.load(store.dispatch);
 
-    return (
-      <div>
-        <Helmet
-          title="POSSESSLESS"
-          titleTemplate="%s"
-          link={[
-            { rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700' },
-          ]}
-          meta={[
-            { 'char-set': 'utf-8' },
-            { name: 'description', content: 'POSSESSLESS' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' }
-          ]}
-        />
-        <MuiThemeProvider muiTheme={muiTheme}>
-          <Provider store={store}>
-            <Router history={browserHistory} onRouteUpdate={this.onRouteUpdate}>
-              <Route path="/" component={Home} />
-              <Route path="/details/:id" component={ItemDetails} />
-              <Route path="/payment" component={Payment} />
-              <Route path="/confirmation" component={Confirmation} />
-            </Router>
-          </Provider>
-        </MuiThemeProvider>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Helmet
+        title="POSSESSLESS"
+        titleTemplate="%s"
+        link={[
+          { rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700' },
+        ]}
+        meta={[
+          { 'char-set': 'utf-8' },
+          { name: 'description', content: 'POSSESSLESS' },
+          { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' }
+        ]}
+      />
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <Provider store={store}>
+          <Router history={browserHistory} onRouteUpdate={onRouteUpdate}>
+            <Route path="/" component={Home} />
+            <Route path="/details/:id" component={ItemDetails} />
+            <Route path="/payment" component={Payment} />
+            <Route path="/confirmation" component={Confirmation} />
+          </Router>
+        </Provider>
+      </MuiThemeProvider>
+    </div>
+  );
+};
 
 export default App;
